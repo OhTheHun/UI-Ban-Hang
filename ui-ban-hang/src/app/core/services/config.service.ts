06ede7c9b10617c93b54526b594e75843be9cf6a
OhTheHun <thaitoan2929.dtc@gmail.com>
2026-05-12 05:48:23 +0700

    update

--- ui-ban-hang/src/app/core/services/config.service.ts
+++ ui-ban-hang/src/app/core/services/config.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
   // You can change this URL in one place
-  private readonly baseUrl = 'https://localhost:7161/api';
+  private readonly baseUrl = 'https://api-backendservice-hmfjfdgyhxfncghf.southeastasia-01.azurewebsites.net/api';
 
   get apiUrl(): string {
     return this.baseUrl;
